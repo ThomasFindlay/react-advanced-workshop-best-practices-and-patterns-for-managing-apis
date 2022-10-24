d1353c4918f04fb9afd897efc95b672a8147fe1a
Thomas <thomasfindlay94@gmail.com>
2022-10-25 00:20:34 +0100

    Merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Quotes from "./components/Quotes.jsx";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <main className="container mx-auto mt-8 ">
-        <Quotes />
-      </main>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <main className="container mx-auto mt-8 ">
+          <Quotes />
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 }
 
