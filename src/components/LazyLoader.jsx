cd909f34adc941f0c221e90a3675bedd8a4ba58f
Thomas <thomasfindlay94@gmail.com>
2022-10-24 22:10:08 +0100

    Start cleanup

--- src/components/LazyLoader.jsx
+++ src/components/LazyLoader.jsx
@@ -1,26 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-
 const LazyLoader = props => {
-  const { show, delay = 1000 } = props;
-  const [showLoader, setShowLoader] = useState(props.show);
-  const timerRef = useRef(null);
-  useEffect(() => {
-    if (!show) {
-      setShowLoader(false);
-      clearTimeout(timerRef.current);
-      return;
-    }
-
-    timerRef.current = setTimeout(() => {
-      setShowLoader(true);
-    }, delay);
-
-    return () => {
-      clearTimeout(timerRef.current);
-    };
-  }, [show, delay]);
-
-  return showLoader ? props.children : null;
+  return null;
 };
 
 export default LazyLoader;
--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,18 +1,11 @@
 import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import Pagination from "./Pagination";
-import LazyLoader from "./LazyLoader";
-
-const IDLE = "IDLE";
-const PENDING = "PENDING";
-const SUCCESS = "SUCCESS";
-const ERROR = "ERROR";
 
 const Quotes = props => {
   const [quotes, setQuotes] = useState([]);
   const [page, setPage] = useState(1);
   const abortRef = useRef({});
-  const [fetchQuotesStatus, setFetchQuotesStatus] = useState(IDLE);
 
   const initFetchQuotes = async page => {
     try {
@@ -21,20 +14,14 @@ const Quotes = props => {
       }
       const controller = new AbortController();
       abortRef.current = controller.abort.bind(controller);
-      setFetchQuotesStatus(PENDING);
       const quotesData = await axios.get(
         `http://localhost:4000/quotes?_page=${page}`,
         {
           signal: controller.signal,
         }
       );
-      const num = Math.random();
-      if (num < 0.5) throw new Error("Oops, something went wrong");
       setQuotes(quotesData.data);
-      setFetchQuotesStatus(SUCCESS);
     } catch (error) {
-      setFetchQuotesStatus(ERROR);
-
       if (error.name === "CanceledError") {
         console.warn(`Request for page ${page} was cancelled`);
       } else {
@@ -79,20 +66,6 @@ const Quotes = props => {
           );
         })}
       </div>
-      <LazyLoader show={fetchQuotesStatus === PENDING}>
-        <p className="text-center">Loading data...</p>
-      </LazyLoader>
-      {fetchQuotesStatus === ERROR ? (
-        <div className="text-center my-4 space-y-4">
-          <p className="text-red-700">Error loading data.</p>
-          <button
-            className="bg-blue-700 text-blue-100 p-4"
-            onClick={() => initFetchQuotes(page)}
-          >
-            Try again
-          </button>
-        </div>
-      ) : null}
       <Pagination page={page} onNext={onNext} onPrev={onPrev} />
     </div>
   );
