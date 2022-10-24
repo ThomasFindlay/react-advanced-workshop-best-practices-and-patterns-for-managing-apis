5a7f11e100d69a0408362b6cb464ea73a45a4ad2
Thomas <thomasfindlay94@gmail.com>
2022-10-25 00:21:13 +0100

    Added custom server.js to prepend quotes on post

---
+++ server.js
@@ -0,0 +1,29 @@
+import jsonServer from "json-server";
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.post("/quotes", async (req, res) => {
+  const { author, quote } = req.body;
+  const id = Date.now();
+  const data = {
+    id,
+    quote,
+    author,
+  };
+
+  const state = router.db.getState();
+  const quotes = [data, ...state.quotes];
+  router.db.setState({
+    ...state,
+    quotes,
+  });
+  return res.json(data);
+});
+
+server.use(router);
+server.listen(4000, () => {
+  console.log("JSON Server is running");
+});
