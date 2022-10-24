dc8132cf7a99fe0e5dd461b99433f1bcd9f0ef19
Thomas <thomasfindlay94@gmail.com>
2022-10-24 17:17:30 +0100

    Start cleanup

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,40 +1,33 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from "react";
 
 const Quotes = props => {
   const [quotes, setQuotes] = useState([]);
-
-  const initFetchQuotes = async () => {
-    const quotesData = await axios.get("http://localhost:4000/quotes");
-    console.log("data", quotesData);
-    setQuotes(quotesData.data);
-  };
-
-  useEffect(() => {
-    initFetchQuotes();
-  }, []);
 
   return (
     <div className="max-w-xl mx-auto">
       <h2 className="font-semibold text-2xl mb-4">Quotes</h2>
       <div>
-        {quotes.map(quote => {
-          return (
-            <blockquote
-              key={quote.id}
-              className="relative p-4 text-xl italic border-l-4"
-            >
-              <p className="mb-4">"{quote.quote}"</p>
-              <cite className="flex items-center justify-center">
-                <div className="flex flex-col items-start">
-                  <span className="mb-1 text-sm italic font-bold">
-                    {quote.author}
-                  </span>
-                </div>
-              </cite>
-            </blockquote>
-          );
-        })}
+        {quotes.length ? (
+          quotes.map(quote => {
+            return (
+              <blockquote
+                key={quote.id}
+                className="relative p-4 text-xl italic border-l-4 mb-8"
+              >
+                <p className="mb-4">"{quote.quote}"</p>
+                <cite className="flex items-center justify-center">
+                  <div className="flex flex-col items-start">
+                    <span className="mb-1 text-sm italic font-bold">
+                      {quote.author}
+                    </span>
+                  </div>
+                </cite>
+              </blockquote>
+            );
+          })
+        ) : (
+          <p>Where are the quotes?</p>
+        )}
       </div>
     </div>
   );
