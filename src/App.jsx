1515dab14db76ce3d693e949ef8ce54c2d18a75d
Thomas <thomasfindlay94@gmail.com>
2022-10-25 16:46:45 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import "./App.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Quotes from "./components/Quotes.jsx";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <main className="container mx-auto mt-8 ">
-          <Quotes />
-        </main>
-      </div>
-    </QueryClientProvider>
+    <div className="App">
+      <main className="container mx-auto mt-8 ">
+        <Quotes />
+      </main>
+    </div>
   );
 }
 
